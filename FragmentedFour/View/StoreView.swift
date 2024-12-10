450eb199969d9f16959b92a326bd8162ec1c6a5d
Brody Roberts <brodyroberts2002@gmail.com>
2024-12-09 19:59:33 -0800

    store UI AGAIN nil colesccing problems

--- FragmentedFour/View/StoreView.swift
+++ FragmentedFour/View/StoreView.swift
@@ -40,6 +40,13 @@ struct StoreView: View {
     @State private var pressedDict: [String: Bool] = [:]
     @State private var pressedPack: Pack?
     @State private var showWindow: Bool = false
+    var pressedPackName: String {
+        pressedPack?.name ?? "nothing"
+    }
+    var pressedPackPrice: Int{
+        pressedPack?.price ?? 0
+    }
+
     
     var body: some View {
         ZStack{
@@ -48,6 +55,13 @@ struct StoreView: View {
             VStack{
                 ZStack{
                     mainColor.ignoresSafeArea()
+                    Color.gray.opacity(showWindow ? 0.6 : 0.0).edgesIgnoringSafeArea(.all)
+                        .onTapGesture {
+                            withAnimation(.spring(response: 0.5, dampingFraction: 0.8)) {
+                                showWindow = false // Dismiss on background tap
+                            }
+                        }
+                    
                     VStack{
                         ZStack(alignment: .top){
                             VStack{
@@ -150,11 +164,13 @@ struct StoreView: View {
                             .frame(height: 30)
                         }
                         .onTapGesture {
-//                            // Bring up purchase view?
-                            pressedDict[pack.name] = true
                             pressedPack = pack
-                            withAnimation(.spring(response: 0.3, dampingFraction: 0.6)){ showWindow = true }
-                            
+                            print("Pressed pack updated: \(pressedPack?.name ?? "nil"), \(pressedPack?.price ?? -1)")
+                            DispatchQueue.main.async {
+                                withAnimation(.spring(response: 0.3, dampingFraction: 0.6)) {
+                                    showWindow = true
+                                }
+                            }
                         }
                     }
                 }
@@ -165,82 +181,74 @@ struct StoreView: View {
                 
             }
             
-            if showWindow {
-                ZStack {
-                                Color.gray.opacity(0.6) // Background overlay
-                                    .edgesIgnoringSafeArea(.all)
-                                    .onTapGesture {
-                                        withAnimation(.spring(response: 0.5, dampingFraction: 0.8)){ showWindow = false }  // Dismiss on background tap
-                                    }
-                                
-                                VStack(spacing: 20) {
-                                    // Close button
-                                    HStack{
-                                        Circle()
-                                            .fill(AppColors.coreBlue)
-                                            .frame(width: 40, height: 40)
-                                            .overlay(
-                                                Image(systemName: "xmark")
-                                                    .foregroundColor(.white)
-                                                    .font(.headline)
-                                            )
-                                            .onTapGesture {
-                                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.4){
-                                                    withAnimation(.spring(response: 0.5, dampingFraction: 0.8)){ showWindow = false }
-                                                }
-                                                
-                                            }
-                                        Spacer()
-                                    }
-                                    
-                                    // Pack pile
-                                    PackPile_View(name: pressedPack!.name)
-                                        .scaleEffect(1.4)
-                                        .frame(height: 250) // Adjust height to center content
-                                    
-                                    // Price and Purchase Button
-                                    HStack(spacing: 20) {
-                                        // Price display
-                                        ZStack {
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .stroke(style: StrokeStyle(lineWidth: 2))
-                                                .foregroundStyle(.black)
-                                            HStack(){
-                                                Spacer()
-                                                    
-                                                Text("\(pressedPack!.price) 💰")
-                                                    .font(.title3.bold())
-                                                    .foregroundStyle(.black)
-                                                    .padding(.leading, 20)
-                                                Spacer()
-                                            }
-                                        }
-                                        .frame(width: 130, height: 50) // Uniform frame for ZStack
-                                        
-                                        ZStack{
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .fill(AppColors.coreBlue)
-                                                
-                                            Text("Purchase")
-                                                .font(.title3.bold())
-                                                .foregroundStyle(.white)
-                                                .fontDesign(.rounded)
-                                        }
-                                        .frame(width: 130, height: 50)
-                                        
-                                            
-                                    }
-                                    .padding(.horizontal)
-
+            // Pop Up View
+            ZStack {
+                
+                VStack(spacing: 20) {
+                    // Close button
+                    HStack {
+                        Circle()
+                            .fill(AppColors.coreBlue)
+                            .frame(width: 40, height: 40)
+                            .overlay(
+                                Image(systemName: "xmark")
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                            )
+                            .onTapGesture {
+                                withAnimation(.spring(response: 0.5, dampingFraction: 0.8)) {
+                                    showWindow = false
                                 }
-                                .padding()
-                                .frame(width: 300, height: 400)
-                                .background(Color.white)
-                                .cornerRadius(20)
-                                .shadow(radius: 10)
-                                
                             }
+                        Spacer()
+                    }
+                    
+                    // Pack pile
+                    PackPile_View(name: pressedPackName)
+                        .scaleEffect(1.4)
+                        .frame(height: 250) // Adjust height to center content
+                    
+                    // Price and Purchase Button
+                    HStack(spacing: 20) {
+                        // Price display
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(style: StrokeStyle(lineWidth: 2))
+                                .foregroundStyle(.black)
+                            HStack {
+                                Spacer()
+                                Text("\(pressedPackPrice) 💰")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(.black)
+                                    .padding(.leading, 20)
+                                Spacer()
+                            }
+                        }
+                        .frame(width: 130, height: 50) // Uniform frame for ZStack
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(AppColors.coreBlue)
+                            
+                            Text("Purchase")
+                                .font(.title3.bold())
+                                .foregroundStyle(.white)
+                                .fontDesign(.rounded)
+                        }
+                        .frame(width: 130, height: 50)
+                    }
+                    .padding(.horizontal)
+                }
+                .padding()
+                .frame(width: 300, height: 400)
+                .background(Color.white)
+                .cornerRadius(20)
+                .shadow(radius: 10)
             }
+            .opacity(showWindow ? 1 : 0)
+            .scaleEffect(showWindow ? 1 : 0.8)
+            .animation(.spring(response: 0.5, dampingFraction: 0.8), value: showWindow)
+
         }
         .navigationBarBackButtonHidden(true)
         .task {
